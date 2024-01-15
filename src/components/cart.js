c8f0f279879a758eec168cb4696638c263779df7
SvetlanaKhobta <svetoch_1980@mail.ru>
2024-01-15 21:53:18 +0900

    <new>

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,36 +1,11 @@
  
 
-// import React from 'react';
-
-// const Cart = ({ cartItems, removeFromCart, updateQuantity, totalPrice }) => {
-//   const handleChangeQuantity = (event, item) => {
-//     const newQuantity = Number(event.target.value);
-//     updateQuantity(item, newQuantity);
-//   };
-
-//   return (
-//     <div>
-//       <h2>Корзина</h2>
-//       <ul>
-//         {cartItems.map((item) => (
-//           <li key={item.id}>
-//             <span>{item.name}</span>
-//             <input
-//               type="number"
-//               min="1"
-//               value={item.quantity}
-//               onChange={(event) => handleChangeQuantity(event, item)}
-//             />
-//             <span>{item.price * item.quantity}</span>
-//             <button onClick={() => removeFromCart(item)}>Удалить</button>
-//           </li>
-//         ))}
-//       </ul>
-//       <div>
-//         <strong>Общая цена: {totalPrice}</strong>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Cart;
+function Cart(){
+    return(
+        <div>
+            <h1>Корзина</h1>
+            
+        </div>
+    )
+}
+export default Cart;
